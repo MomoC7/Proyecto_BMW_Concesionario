86de48fd0bf12cf3455beb491b043b433daaee93
MomoC7 <juancpm797@gmail.com>
2025-04-02 13:59:36 -0500

    Archivo añadido
    -Se agregó el archivo de la base de datos MSQL

---
+++ base_de_datos_MSQL.sql
@@ -0,0 +1,32 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.41 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para bmw_concesionario
+CREATE DATABASE IF NOT EXISTS `bmw_concesionario` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bmw_concesionario`;
+
+-- Volcando datos para la tabla bmw_concesionario.cars: ~0 rows (aproximadamente)
+INSERT INTO `cars` (`id_auto`, `brand`, `model`, `yearm`, `price`) VALUES
+	(1, 'BMW', 'X4', 2018, 140000000),
+	(5, 'BMW', 'M3 ', 2003, 60000000),
+	(6, 'BMW', 'Serie 4 Convertible', 2024, 419000000);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
